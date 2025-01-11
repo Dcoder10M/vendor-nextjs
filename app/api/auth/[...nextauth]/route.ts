e1fbea8e22c4a6ca7573316e30105e6a980d36f1
Dcoder10M <div.singh.rathore.ff@gmail.com>
2025-01-11 14:21:40 +0530

    prisma singleton

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,4 @@
+import prisma from '@/db'
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
@@ -10,6 +11,36 @@ const handler = NextAuth({
   ],
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
+    async signIn({ user, profile }) {
+      if (!profile || !profile.sub) {
+        console.error('Missing Google profile or sub')
+        return false
+      }
+    
+      const googleId = profile.sub
+    
+      try {
+        const existingUser = await prisma.user.findUnique({
+          where: { id: googleId },
+        })
+    
+        if (!existingUser) {
+          await prisma.user.create({
+            data: {
+              id: googleId,
+              name: user.name || 'Unknown User',
+              email: user.email || `user@example.com`,
+              image: user.image,
+            },
+          })
+        }
+      } catch (error) {
+        console.error('Error saving user to database:', error)
+        return false
+      }
+    
+      return true
+    },
     async session({ session, token }) {
       if (session.user) {
         ;(session.user as { id?: string }).id = token.sub
